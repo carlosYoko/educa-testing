5b1049d61b1fcf16e27053497136fb4fdd525315
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-09-19 20:14:18 +0200

    created model and validationattribute

---
+++ EducaTesting.Domain/Course.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EducaTesting.Domain
+{
+    public class Course
+    {
+        [Key]
+        public Guid CourseId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [DateInFuture]
+        public DateTime? DatePublication { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DateCreation { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ EducaTesting.Domain/DateInFutureAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EducaTesting.Domain
+{
+    public class DateInFutureAttribute : ValidationAttribute
+    {
+        private readonly Func<DateTime> _datetimeNowProvider;
+
+        public DateInFutureAttribute() : this(() => DateTime.Now)
+        { }
+
+        public DateInFutureAttribute(Func<DateTime> datetimeNowProvider)
+        {
+            _datetimeNowProvider = datetimeNowProvider;
+            ErrorMessage = "La fecha tiene que ser de hoy en adelante";
+        }
+
+        public override bool IsValid(object? value)
+        {
+            bool isValid = false;
+            if (value is DateTime dateTime)
+            {
+                isValid = dateTime > _datetimeNowProvider();
+            }
+
+            return isValid;
+        }
+    }
+}
