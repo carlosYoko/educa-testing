ef37f5fcdef5e2c896f934c2e9f36c0b45714dfe
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-09-19 21:53:56 +0200

    query wirth cqrs params

---
+++ EducaTesting.Application/Courses/GetCourseByIdQuery.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using EducaTesting.Application.DTO;
+using EducaTesting.Domain;
+using EducaTesting.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducaTesting.Application.Courses
+{
+    public class GetCourseByIdQuery
+    {
+        public class GetCourseByIdQueryRequest : IRequest<CourseDTO>
+        {
+            public Guid Id;
+        }
+
+        public class GetCourseByIdQueryHandler : IRequestHandler<GetCourseByIdQueryRequest, CourseDTO>
+        {
+            private readonly EducaTestingDbContext _context;
+            private readonly IMapper _mapper;
+
+            public GetCourseByIdQueryHandler(EducaTestingDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<CourseDTO> Handle(GetCourseByIdQueryRequest request, CancellationToken cancellationToken)
+            {
+                var course = await _context.Courses.FirstOrDefaultAsync(x => x.CourseId == request.Id);
+                var courseDTO = _mapper.Map<Course, CourseDTO>(course);
+                return courseDTO;
+            }
+        }
+    }
+}
