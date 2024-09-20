967dc3e49349a45c8294d653520af366f5a80f26
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-09-20 18:58:20 +0200

    created fake dbcontext

---
+++ EducaTesting.Application.NUnitTests/Courses/GetCourseQueryNUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using AutoFixture;
+using EducaTesting.Domain;
+using EducaTesting.Persistence;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace EducaTesting.Application.Courses
+{
+    [TestFixture]
+    public class GetCourseQueryNUnitTests
+    {
+        private GetCourseQuery.GetCourseQueryHandler handlerAllCourses;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var fixture = new Fixture();
+            var courseRecords = fixture.CreateMany<Course>().ToList();
+
+            courseRecords.Add(fixture.Build<Course>()
+                .With(x => x.CourseId, Guid.Empty)
+                .Create()
+                );
+
+            var options = new DbContextOptionsBuilder<EducaTestingDbContext>()
+                .UseInMemoryDatabase(databaseName: $"EducaTestingDbContext-{Guid.NewGuid()}")
+                .Options;
+
+            var educaTestingDbContext = new EducaTestingDbContext(options);
+            educaTestingDbContext.AddRange(courseRecords);
+            educaTestingDbContext.SaveChanges();
+
+        }
+
+
+        [Test]
+        public void GetCourseQueryHandler_QueryCourses_ReturnsTrue()
+        {
+            // Act
+
+            // Arrange
+
+            // Assert
+        }
+    }
+}
